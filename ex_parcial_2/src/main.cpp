5c53954e1ffe928cde0f1515adf7b53752cfe81a
Miguel <miguel_angel_bucio@hotmail.com>
2020-10-30 00:40:32 +0000

    tray 1

--- ex_parcial_2/src/main.cpp
+++ ex_parcial_2/src/main.cpp
@@ -14,9 +14,9 @@
 #define antebrazo 14.641
 
 int main(){
-    float x0 = 3;
-    float y0 = 4;
-    float z0 = 5;
+    float x0 = 7;
+    float y0 = 15;
+    float z0 = 7;
     float delta = 1;
     coordinates_time_t coor_tmp;
     std::ofstream wr_file;
@@ -24,7 +24,8 @@ int main(){
     distance_t w_1 = {.dx=-3.95, .dy=6.44, .dz=0.0304, .dist=7.5549};
     distance_t w_2 = {.dx=-1.161, .dy=8.85, .dz=0.9696, .dist=9.0474};
     distance_t w_3 = {.dx=-1.161, .dy=17.89, .dz=0.9796, .dist=17.989};
-    distance_t w_4 = {.dx=-3.95, .dy=32.541, .dz=-13.153, .dist=32.8062};
+    distance_t w_4 = {.dx=3.95, .dy=32.541, .dz=-13.153, .dist=32.8062};
+    A01633021 *robot = new A01633021(w_1, w_2, w_3, w_4, altura, brazo, antebrazo);
     //Archivos para guardar datos
     std::string trajectory_files[5]     = {"data/trajectory_1.txt", "data/trajectory_2.txt", "data/trajectory_3.txt", "data/trajectory_4.txt", "data/trajectory_5.txt"};
     std::string joints_files[5]         = {"data/joints_1.txt", "data/joints_2.txt", "data/joints_3.txt", "data/joints_4.txt", "data/joints_5.txt"};
@@ -127,8 +128,7 @@ int main(){
         wr_file << t << ", " << coor_tmp.coordinates.x << ", " << coor_tmp.coordinates.y << ", " << coor_tmp.coordinates.z <<"\n";
     }
     wr_file.close();
-    std::cout << "Creating robot with parameters\n";
-    A01633021 *robot = new A01633021(w_1, w_2, w_3, w_4, altura, brazo, antebrazo);
+
     
     //Process trajectories
     for (int idx=0; idx<5; idx++){
